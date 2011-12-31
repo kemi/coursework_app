76dacad55b20676545111d2d588acbfb830accb8
kemi <elsiekem08@yahoo.com>
2011-12-31 16:40:45 +0000

    Done with static-pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,34 @@
 class PagesController < ApplicationController
   def home
+   @title = "Home"
   end
 
   def enquiry_service
+     @title = "Enquiry Service"
   end
 
   def services_support
+     @title = "Services and support"
   end
 
   def information_advice
+     @title = "Information and advice"
   end
 
   def recording_tools
+     @title = "Recording Tools"
   end
 
   def submit_your_sightings
+     @title = "Submit your sightings"
   end
 
   def about
+     @title = "About"
   end
 
   def contact
+     @title = "Contact"
   end
 
 end
