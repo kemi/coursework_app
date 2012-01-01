5fc2d29f0ec82520e3e535b1c6d21fbdfde55f9c
kemi <elsiekem08@yahoo.com>
2012-01-01 20:42:45 +0000

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,16 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-if defined?(Bundler)
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+#if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+ # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
-end
+#end
 
 module CourseworkApp
   class Application < Rails::Application
@@ -44,5 +48,7 @@ module CourseworkApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+ # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
   end
 end
