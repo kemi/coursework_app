c3c0dc3a258c0955cdca97c6a97f1b1a50850cdb
kemi <elsiekem08@yahoo.com>
2012-01-07 09:33:43 +0000

    Finished part of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,37 +1,47 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+#require 'digest'
 class User < ActiveRecord::Base
-attr_accessor :password
-attr_accessible :name, :email, :password, :password_confirmation
+ attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
-email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-validates :name, :presence => true
-                 :length => { :maximum => 50 }
-validates :email, :presence=> true
-                  :format => { :with => email_regex }
-                  :uniqueness => { :case_sensitive => false }
- # Automatically create the virtual attribute 'password_confirmation'.
-validates :password, :presence     => true,
+ validates :name, :presence => true
+                  
+ validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                     :uniqueness => { :case_sensitive => false }
+# Automatically create the virtual attribute 'password_confirmation'.
+  validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }
- before_save :encrypt_password
-
+  before_save :encrypt_password
  # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
-     encrypted_password == encrypt(submitted_password)
+    encrypted_password == encrypt(submitted_password)
     # Compare encrypted_password with the encrypted version of
     # submitted_password.
   end
-def self.authenticate(email, submitted_password)
+ def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
   end
- def self.authenticate_with_salt(id, cookie_salt)
-    user = find_by_id(id)
-    (user && user.salt == cookie_salt) ? user : nil
-  end
+  def self.authenticate_with_salt(id, cookie_salt)
+     user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+end
 
-  private
+ private
 
     def encrypt_password
       self.salt = make_salt unless has_password?(password)
@@ -50,5 +60,7 @@ def self.authenticate(email, submitted_password)
       Digest::SHA2.hexdigest(string)
     end
 
-# validates :name, :presence => true
+
+		  
 end
+
